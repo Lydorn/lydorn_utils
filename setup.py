3f1a1c38650d5ab9f0d3d2e5be2e85772b0ecd65
lydorn <nicolas.jp.girard@gmail.com>
2020-02-12 22:28:09 +0900

    Use markdown for READMEs

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ import setuptools
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
 # The text of the README file
-README = (HERE / "README.rst").read_text()
+README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setuptools.setup(
     name="lydorn_utils",
     version="0.0.1",
     description="Various utilities for deep learning projects",
     long_description=README,
-    long_description_content_type="text/x-rst",
+    long_description_content_type="text/markdown",
     author="Nicolas Girard",
     author_email="nicolas.jp.girard@gmail.com",
     license="INRIA",
