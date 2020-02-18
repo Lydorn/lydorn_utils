d176d3efc29a41c4adbbe576ee956c5ac8a039bf
lydorn <nicolas.jp.girard@gmail.com>
2020-02-18 09:04:20 +0900

    Added getter to avg meter

--- lydorn_utils/math_utils.py
+++ lydorn_utils/math_utils.py
@@ -66,6 +66,9 @@ class AverageMeter(object):
         self.count += n
         self.avg = self.sum / self.count
 
+    def get_avg(self):
+        return self.avg
+
     def __str__(self):
         fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
         return fmtstr.format(**self.__dict__)
